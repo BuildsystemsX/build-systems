3f2a855ee32727a6f637ea18261e1764294ede0a
vinzenz <vinzenz.trimborn@tum.de>
2023-05-29 12:37:02 +0200

    Added Mono

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,27 @@ import Head from "next/head";
 import localFont from 'next/font/local';
 import * as React from 'react';
 
-const reallySansLarge = localFont({
+const diatypeMedium = localFont({
     src: '../fonts/ABCDiatype-Medium.woff2',
     weight: '900',
     style: 'normal',
     display: 'swap',
-    variable: '--Medium'
+    variable: '--DiatypeMedium'
+});
+
+const diatypeMediumMono = localFont({
+    src: '../fonts/ABCDiatypeMono-Medium.woff2',
+    weight: '900',
+    style: 'normal',
+    display: 'swap',
+    variable: '--DiatypeMediumMono'
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     // Set the CSS variable on <body> to ensure Plasmic Studio has access to the variable.
     React.useEffect(() => {
-        document.body.classList.add(reallySansLarge.variable);
+        document.body.classList.add(diatypeMedium.variable);
+        document.body.classList.add(diatypeMediumMono.variable);
     }, []);
   return (
     <PlasmicRootProvider Head={Head}>
